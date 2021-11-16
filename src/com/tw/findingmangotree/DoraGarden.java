ad6bad960d60d184b3ce941a1bf1831a647cea56
admin <charitha060@gmail.com>
2021-11-16 22:41:01 +0530

    REFACTOR. method name from foundMango to foundMangotree

--- src/com/tw/findingmangotree/DoraGarden.java
+++ src/com/tw/findingmangotree/DoraGarden.java
@@ -6,20 +6,20 @@ public class DoraGarden {
 
     public static void main(String[] args) {
 
-        Trees trees=new Trees();
+        Trees trees = new Trees();
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter number of rows:");
-        int numberOfRows= scanner.nextInt();
+        int numberOfRows = scanner.nextInt();
 
         System.out.println("Enter number of columns:");
-        int numberOfColumns= scanner.nextInt();
+        int numberOfColumns = scanner.nextInt();
 
         System.out.println("Enter tree number");
-        int treeNumber= scanner.nextInt();
+        int treeNumber = scanner.nextInt();
 
-        System.out.println(trees.foundMango(numberOfRows,numberOfColumns,treeNumber));
+        System.out.println(trees.foundMangoTree(numberOfRows, numberOfColumns, treeNumber));
     }
 }
 
--- src/com/tw/findingmangotree/Trees.java
+++ src/com/tw/findingmangotree/Trees.java
@@ -2,33 +2,30 @@ package com.tw.findingmangotree;
 
 public class Trees {
 
-        public String foundMango(int numberOfRows, int numberOfColumns, int treeNumber){
-            int flag=0;
-            for(int i=1;i<=numberOfRows;i++)
-            {
-                if(i==1)
-                {
-                    for(int j=1;j<=numberOfColumns;j++)
-                    {
-                        if(treeNumber==i*j) {
-                            flag = 1;
-                            break;
-                        }
-                    }
-                }
-                if(i==numberOfRows) {
-                    for (int j = 1; j <= numberOfColumns; j++) {
-                        if (treeNumber == i * j || treeNumber == ((i * j)) + 1) {
-                            flag = 1;
-                            break;
-
-                        }
+    public String foundMangoTree(int numberOfRows, int numberOfColumns, int treeNumber) {
+        int flag = 0;
+        for (int i = 1; i <= numberOfRows; i++) {
+            if (i == 1) {
+                for (int j = 1; j <= numberOfColumns; j++) {
+                    if (treeNumber == i * j) {
+                        flag = 1;
+                        break;
                     }
                 }
             }
-            if(flag==1)
-                return "yes";
-            else
-                return "No";
+            if (i == numberOfRows) {
+                for (int j = 1; j <= numberOfColumns; j++) {
+                    if (treeNumber == i * j || treeNumber == ((i * j)) + 1) {
+                        flag = 1;
+                        break;
+
+                    }
+                }
+            }
         }
+        if (flag == 1)
+            return "yes";
+        else
+            return "No";
+    }
 }
