cb8d7be279ea677b248743884e95cbf0a2c85c3c
admin <charitha060@gmail.com>
2021-11-16 22:06:03 +0530

    ADD. DoraGarden class

---
+++ src/com/tw/findingmangotree/DoraGarden.java
@@ -0,0 +1,25 @@
+package com.tw.findingmangotree;
+
+import java.util.Scanner;
+
+public class DoraGarden {
+
+    public static void main(String[] args) {
+
+        Trees trees=new Trees();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of rows:");
+        int numberOfRows= scanner.nextInt();
+
+        System.out.println("Enter number of columns:");
+        int numberOfColumns= scanner.nextInt();
+
+        System.out.println("Enter tree number");
+        int treeNumber= scanner.nextInt();
+
+        System.out.println(trees.foundMango(numberOfRows,numberOfColumns,treeNumber));
+    }
+}
+
